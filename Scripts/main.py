220aecd337d1c823a6ff2ccd3370083bde85f151
Kaspara Skovli Gåsvær <kasparag@gmail.com>
2021-09-14 13:33:07 +0200

    plots of num tweet dist

--- Scripts/main.py
+++ Scripts/main.py
@@ -14,3 +14,16 @@ path = "./experiment6/experiment_6/"
 my_class = Slices(path)
 #print(my_class.G)
 #print(my_class.slices)
+
+"""
+x = [1,2,3,4]
+l = [2,8,5,6]
+names = ["one", "two", "three","four"]
+zeros = [0 for i in range(len(l))]
+plt.vlines(x = x,ymin = zeros,ymax = l,lw=40)
+for i in range(len(l)):
+    plt.text(x[i]-0.1, l[i], names[i])
+plt.ylim(ymin=0)
+plt.ylabel("$\\frac{\mathrm{Number\,of\,tweets\,<\,2020}}{\mathrm{Total\,number\,of\,tweets\,in\,slice}}$", fontsize = 12)
+plt.show()
+"""
--- Scripts/parse_slices.py
+++ Scripts/parse_slices.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import sys
 import datetime as dt
+from matplotlib.dates import date2num
 
 
 
@@ -16,6 +17,7 @@ class Slices:
         self.all_slices = []
         self.graphs = []
         self.slices = {} #Add first and llast date as keys
+        self.attributes_to_slice_set = {}
 
 
         for file in all_files:
@@ -34,8 +36,10 @@ class Slices:
             self.make_graph()
             self.find_timeline()
             self.find_num_nodes()
-
-        self.find_num_deleted_users()
+        self.find_timeline_of_set()
+        #self.plot_dates_dist()
+        #self.find_num_deleted_users()
+        self.plot_tweets_before_2020()
 
     def make_node_attributes(self):
         filename = self.path + "labels_" + str(self.slice_num) + ".csv"
@@ -74,6 +78,106 @@ class Slices:
         self.slices[str(self.slice_num)] = {'graph':self.G}
         self.slices[str(self.slice_num)]['node_attributes'] = self.node_attributes
 
+    def plot_tweets_before_2020(self):
+        tweets = []
+        for slice in self.slices:
+            tweets.append(0)
+        tweets.append(0)
+
+        boundary = dt.datetime(2020,1,1)
+        for slice in self.slices:
+            nodes = self.slices[slice]['node_attributes']
+            num_nodes = self.slices[slice]['num_nodes']
+            #print("Number of nodes in slice %s = %i" %(slice,num_nodes))
+            for n in nodes:
+                date = dt.datetime.strptime((nodes[n]['date']),"%Y-%m-%dT%H:%M:%S.%f")
+                if date < boundary:
+                    tweets[int(slice)] += 1
+            #tweets[int(slice)]/=num_nodes
+
+        tweets.pop(0)
+        #print(tweets)
+
+        names = sorted([slice for slice in self.slices])
+        x_pos = [int(i) for i in names]
+        zeros = [0 for i in range(len(tweets))]
+
+        plt.vlines(x = x_pos, ymin = zeros, ymax = tweets, lw = 2)
+        #for i in range(len(tweets)):
+        #    plt.text(x_pos[i],tweets[i],names[i])
+        plt.ylim(ymin=0)
+        plt.title("Tweets before 2020",fontsize = 14)
+        plt.xlabel("Slice number", fontsize = 12)
+        #plt.ylabel("$\\frac{\mathrm{Number\,of\,tweets\,<\,2020}}{\mathrm{Total\,number\,of\,tweets\,in\,slice}}$", fontsize = 14)
+        plt.ylabel("Number of tweets before 2020", fontsize = 12)
+        plt.xticks(fontsize=12)
+        plt.yticks(fontsize=10)
+        plt.savefig("./experiment6/plots/num_tweets_before_2020/bins.jpg")
+        plt.clf()
+
+
+
+
+
+    def plot_dates_dist(self):
+        three_months = dt.timedelta(days = 90)
+        for slice in self.slices:
+            start = dt.datetime.strptime(self.slices[slice]['start_date'],"%Y-%m-%d-%H-%M-%S-%f")
+            end = dt.datetime.strptime(self.slices[slice]['end_date'],"%Y-%m-%d-%H-%M-%S-%f")
+            date_list = []
+            nodes = self.slices[slice]['node_attributes']
+            for n in nodes:
+                date_list.append(dt.datetime.strptime((nodes[n]['date']),"%Y-%m-%dT%H:%M:%S.%f"))
+
+            date_list.remove(start)
+            date_list_num = date2num(date_list)
+            bins = np.arange(date_list_num.min(), date_list_num.max()+1, 90)
+
+            plt.title("Slice "+slice,fontsize = 14)
+            plt.xlabel("Date", fontsize = 14)
+            plt.ylabel("Number of tweets", fontsize = 14)
+            plt.xticks(fontsize=12)
+            plt.yticks(fontsize=12)
+            plt.hist(date_list,bins = bins)
+            plt.savefig("./experiment6/plots/num_tweets_three_month_bins/Slice_" +slice + ".jpg")
+            plt.clf()
+
+    def find_timeline_of_set(self):
+        first_date = "2100-06-10-20-38-27-000"
+        first_date = dt.datetime.strptime(first_date,"%Y-%m-%d-%H-%M-%S-%f")
+        first_node = 0
+        last_node = 0
+        last_date = "1900-06-10-20-38-27-000"
+        last_date = dt.datetime.strptime(last_date,"%Y-%m-%d-%H-%M-%S-%f")
+        first_slice = '1'
+        last_slice = '1'
+        for slice in self.slices:
+            date = self.slices[slice]['start_date']
+            date = dt.datetime.strptime(date,"%Y-%m-%d-%H-%M-%S-%f")
+            if date < first_date:
+                first_date = date
+                first_node = self.slices[slice]['start_date_node_index']
+                first_slice = slice
+            date = self.slices[slice]['end_date']
+            date = dt.datetime.strptime(date,"%Y-%m-%d-%H-%M-%S-%f")
+            if date > last_date:
+                last_date = date
+                last_node = self.slices[slice]['end_date_node_index']
+                last_slice = slice
+
+        first_date = dt.datetime.strftime(first_date,"%Y-%m-%d-%H-%M-%S-%f")
+        last_date = dt.datetime.strftime(last_date,"%Y-%m-%d-%H-%M-%S-%f")
+        self.attributes_to_slice_set['start_date'] = first_date
+        self.attributes_to_slice_set['end_date'] = last_date
+        self.attributes_to_slice_set['start_date_node_index'] = first_node
+        self.attributes_to_slice_set['end_date_node_index'] = last_node
+        self.attributes_to_slice_set['start_slice'] = first_slice
+        self.attributes_to_slice_set['end_slice'] = last_slice
+
+
+
+
+
 
     def find_timeline(self):
 
@@ -88,10 +192,10 @@ class Slices:
         for n in self.node_attributes:
             date = self.node_attributes[n]["date"]
             date = dt.datetime.strptime(date,"%Y-%m-%dT%H:%M:%S.%f")
-            if date < first_date:
+            if date <= first_date:
                 first_date = date
                 first_node = n
-            if date > last_date:
+            if date >= last_date:
                 last_date = date
                 last_node = n
 
