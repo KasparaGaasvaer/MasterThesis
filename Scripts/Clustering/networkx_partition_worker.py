f80344ae876f4efff793abdb6b3e237a502add88
Kaspara Skovli Gåsvær <kasparag@gmail.com>
2021-11-04 16:31:53 +0100

    nothing

--- Scripts/Clustering/networkx_partition_worker.py
+++ Scripts/Clustering/networkx_partition_worker.py
@@ -13,7 +13,7 @@ class NetworkX_Partition_Worker():
     def __init__(self, path, method):
         self.path_to_partitions = path
         self.path_to_stats_results = "./experiment6/statistics/"
-        
+
 
         if method == "louvain":
             self.filename_jumpers = "all_slices_partition_jumper_stats_louvain.py"
@@ -31,8 +31,8 @@ class NetworkX_Partition_Worker():
 
         #self.nx_compare_partitions()
         self.nx_count_jumpers()
-        
-        
+
+
 
     def nx_compare_partitions(self):
 
@@ -46,7 +46,7 @@ class NetworkX_Partition_Worker():
         keys_2 = list(s_2.keys())
         for i in range(len(keys_1)):
             keys_1[i] = int(keys_1[i])
-        
+
         keys_1 = sorted(keys_1)
 
         for i in range(len(keys_2)):
@@ -82,7 +82,7 @@ class NetworkX_Partition_Worker():
 
             keys_i = len(s_i.keys())
             keys_ip1 = len(s_ip1.keys())
-    
+
             for k_i in range(keys_i):
                 list_i = s_i[str(k_i)]
                 list_ip1 = s_ip1[str(k_i)]
@@ -91,17 +91,15 @@ class NetworkX_Partition_Worker():
                     for n in list_i:
                         if not n in list_ip1:
                             num_jumpers[i] += 1
-                
+
             num_nodes_ip1 = sum(len(v) for v in s_ip1.values())
            # print('NODES',num_nodes_ip1)
             ratio_jumpers_partitions[i] = num_jumpers[i]/keys_ip1
             #print('KEYS',keys_ip1)
             ratio_jumpers_nodes[i] = num_jumpers[i]/num_nodes_ip1
 
-        
+
         with open(self.path_to_stats_results + self.filename_jumpers,"w") as outfile:
             outfile.write(f'Slice_i to Slice_ip1| Total number of jumpers |  Ratio jumpers/total number of nodes in slice_ip1 | Ratio jumpers/total number of partitions in slice_ip1\n')
             for i in range(1,self.num_total_slices):
                 outfile.write(f'       {i}-{i+1:<20} {num_jumpers[i]:<40} {ratio_jumpers_nodes[i]:.6f} {ratio_jumpers_partitions[i]:45.6f}\n')
-    
-        
