21a27d4282ca30d0a9f2ff145b18fa86601c6fd5
Kaspara Skovli Gåsvær <kasparag@srl-login1.cm.cluster>
2021-12-01 13:14:36 +0100

    notes

--- Scripts/Slices_Contacts_pseudo.py
+++ Scripts/Slices_Contacts_pseudo.py
@@ -11,12 +11,12 @@ def create_slices(I,a, N, E, Sc, e):
     N = 0
     K_holder = [k_old,k_new]
     j = 1
-    while [len( E[n] ) <= K[n] for n in N]:
-        Sc[j] += e[i]
-        if [any len(E[n]) > K[n] for n in N]: #Can insted just check the affected nodes by e_i
-            check_agreement([nodes affected by e_i])
-            if agreement:
-                Sc[j+1] = Sc[j] - find_eno() 
+    while something:
+        Sc[j] += e[i] # e[u] = (u1,u2)
+        if (len(n[u1]) > K[u1] or len(n[u2]) > K[u2]): #Have to determine which one as well
+            find e_oldest of u, identify v #where v is the node connected through the oldest edge of u
+            if (len(n[v]) > K[v]):
+                Sc[j+1] = Sc[j] - e_oldest
                 K_holder[j-1] = Mean_num_edges() #func that returns list*N of num edges to nodes
                 j+= 1
         else:
