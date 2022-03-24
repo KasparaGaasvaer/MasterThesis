71558f0b0de5df1bece38e360ea1186f2b156ddc
Kaspara <kaspara@eduroam-193-157-214-94.wlan.uio.no>
2022-03-24 15:40:23 +0100

    tull

--- copy_files.sh
+++ copy_files.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-CURRENT_PATH="/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis/"
 TARGET_PATH="/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/daniel_repo/plots/"
-ALL_FILES=$(find ${CURRENT_PATH} -name "*plots*")
 
-cp -r ${ALL_FILES} ${TARGET_PATH}
+cp -r "/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis//Scripts/experiment6/plots/" "${TARGET_PATH}experiment6/"
+cp -r "/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis//Scripts/experiment7/plots/" "${TARGET_PATH}experiment7/"
+cp -r "/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis//Scripts/experiment8/plots/" "${TARGET_PATH}experiment8/"
+cp -r "/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis//Scripts/experiment9/plots/" "${TARGET_PATH}experiment9/"
+cp -r "/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis//Scripts/experiment12/experiment_12/k_800/plots" "${TARGET_PATH}k800/"
+cp -r "/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis//Scripts/experiment12/experiment_12/k_600/plots" "${TARGET_PATH}k600/"
+cp -r "/Users/kaspara/Documents/MASTER_GIT_GAMMEL_MAC/MasterThesis//Scripts/Clustering/PlotScripts/plots_from_notebook" "${TARGET_PATH}notebook_plots/"
